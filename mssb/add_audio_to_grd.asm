5378aff50d356847ab76aa08721ad8e58383c848
Micah Russell <108246578+MORImementos@users.noreply.github.com>
2024-05-23 21:11:57 -0500

    Update add_audio_to_grd.asm
    Add comments

--- mssb/add_audio_to_grd.asm
+++ mssb/add_audio_to_grd.asm
@@ -4,10 +4,10 @@ stb r0, 0x1bd1(r5)
 stwu sp,-0x80 (sp) #Push stack, make space for 29 registers
 stmw r3, 0x8 (sp)
 
-li r3, 0x159
+li r3, 0x159 # argument for GRD audio
 lis r12, 0x806C
-ori r12, r12, 0xF328
-mtctr r12
+ori r12, r12, 0xF328 # function address
+mtctr r12 # call sfx function
 bctrl 
 
 lmw r3, 0x8 (sp)
